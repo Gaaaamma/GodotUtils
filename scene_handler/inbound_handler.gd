836c5f33d8b165629f38db0dbea9b13f3ac14a9c
Gaaaamma <ytching552022@gmail.com>
2026-01-19 22:03:23 +0800

    Update: Inbound - use global_position instead is more accurate

--- scene_handler/inbound_handler.gd
+++ scene_handler/inbound_handler.gd
@@ -1,6 +1,6 @@
 class_name InboundHandler extends Node2D
 
-var inbound_position: Dictionary[String, Vector2] = {}
+var inbound_global_position: Dictionary[String, Vector2] = {}
 var inbound_direction: Dictionary[String, Vector2] = {}
 
 func _ready() -> void:
@@ -12,12 +12,14 @@ func _ready() -> void:
 func collect_inbound_information():
 	for child in get_children():
 		if child is Inbound:
-			inbound_position[child.name] = child.position
+			inbound_global_position[child.name] = child.global_position
 			inbound_direction[child.name] = child.direction.normalized()
 
-## Use Inbound node name to search spawn position
+
+## Use Inbound node name to search spawn position (global_position)
 func get_spawn_position(location: String) -> Vector2:
-	return inbound_position.get(location)
+	return inbound_global_position.get(location)
+
 
 ## Use Inbound node name to search spawn direction
 func get_spawn_direction(location: String) -> Vector2:
